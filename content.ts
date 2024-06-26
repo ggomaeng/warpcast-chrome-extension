5763f07e539047405504705ae1d8d7d11f206217
Austin Sambrano <Austinsam210@gmail.com>
2024-06-25 21:06:58 -0400

    Added warpcast theme features.

--- content.ts
+++ content.ts
@@ -1,6 +1,12 @@
-import type { PlasmoCSConfig } from "plasmo"
+import type { PlasmoCSConfig } from "plasmo";
 
-import { Storage } from "@plasmohq/storage"
+
+
+import { Storage } from "@plasmohq/storage";
+
+
+
+
 
 export const config: PlasmoCSConfig = {
   matches: ["https://warpcast.com/*"]
@@ -52,6 +58,124 @@ window.addEventListener("load", async () => {
     gifPFP: (c) => {
       const checked = c.newValue
       replaceImgSrc(checked)
+    }
+  })
+
+  async function changeFont(selector: string, color: string) {
+    if (color === "") return
+
+    let style = document.getElementById(
+      `custom-font-style-${selector}`
+    ) as HTMLStyleElement
+
+    const css = `
+    ${selector} {
+      color: ${color} !important;
+    }
+  `
+    if (!style) {
+      style = document.createElement("style")
+      style.id = `custom-font-style-${selector}`
+      style.appendChild(document.createTextNode(css))
+      document.head.appendChild(style)
+    } else {
+      style.textContent = css
+    }
+  }
+
+  async function getMainColorValue() {
+    const value: string = (await storage.get("fontColorMain")) ?? ""
+    return value
+  }
+
+  changeFont(".text-default", await getMainColorValue())
+
+  storage.watch({
+    fontColorMain: (c) => {
+      const color = c.newValue
+      changeFont(".text-default", color)
+    }
+  })
+
+  async function getSecondColorValue() {
+    const value: string = (await storage.get("fontColorSecond")) ?? ""
+    return value
+  }
+
+  changeFont("#root, body, html", await getSecondColorValue())
+
+  storage.watch({
+    fontColorSecond: (c) => {
+      const color = c.newValue
+      changeFont("#root, body, html", color)
+    }
+  })
+
+  function setBackgroundImage(url: string, innerBack: string | boolean) {
+    if (window.location.hostname === "warpcast.com") {
+      let rootDiv = document.querySelector("div[data-rk]")
+
+      if (innerBack) {
+        let interval = 100
+        let intervalId
+
+        const checkAndSetBackground = () => {
+          rootDiv = document.querySelector(
+            ".flex.min-h-screen.flex-row.justify-center > .h-full.w-full.shrink-0.justify-center.sm\\:mr-4.sm\\:w-\\[540px\\].lg\\:w-\\[620px\\] > div.h-full.w-full"
+          )
+
+          if (rootDiv instanceof HTMLDivElement) {
+            const expandedDiv = rootDiv.querySelector(
+              "div.h-full.min-h-screen.border-default.sm\\:border-x.flex.flex-col.lg\\:w-\\[1023px\\].lg\\:flex-row"
+            )
+
+            if (expandedDiv) {
+              rootDiv = expandedDiv
+            }
+
+            if (rootDiv instanceof HTMLDivElement) {
+              interval = 1000
+              clearInterval(intervalId)
+              rootDiv.style.backgroundImage = `url("${url}")`
+              rootDiv.style.backgroundAttachment = "fixed"
+              rootDiv.style.backgroundPosition = "center"
+              rootDiv.style.backgroundSize = "cover"
+              rootDiv.style.backgroundRepeat = "no-repeat"
+
+              intervalId = setInterval(checkAndSetBackground, interval)
+            }
+          }
+        }
+
+        intervalId = setInterval(checkAndSetBackground, interval)
+      } else {
+        if (rootDiv instanceof HTMLDivElement) {
+          rootDiv.style.backgroundImage = `url("${url}")`
+          rootDiv.style.backgroundAttachment = "fixed"
+          rootDiv.style.backgroundPosition = "center"
+          rootDiv.style.backgroundSize = "cover"
+          rootDiv.style.backgroundRepeat = "no-repeat"
+        }
+      }
+    }
+  }
+
+  async function getBackgroundImage() {
+    const value: string = (await storage.get("imageUrl")) ?? ""
+    return value
+  }
+
+  async function getInnerBackground() {
+    const value: string | boolean = (await storage.get("innerBackground")) ?? ""
+    return value
+  }
+
+  setBackgroundImage(await getBackgroundImage(), await getInnerBackground())
+
+  storage.watch({
+    imageUrl: async (c) => {
+      const url = c.newValue
+      setBackgroundImage(url, await getInnerBackground())
     }
   })
 })
--- popup.tsx
+++ popup.tsx
@@ -1,10 +1,40 @@
-import { Storage } from "@plasmohq/storage"
+import { Storage } from "@plasmohq/storage";
+
+
+
+
+
 
 import "./style.css"
 
+import { useRef } from "react"
+
 import { useStorage } from "@plasmohq/storage/hook"
 
+import { PopoverPicker } from "~components/PopoverPicker"
+
 function IndexPopup() {
+  const [colorMain, setColorMain] = useStorage<string>({
+    key: "fontColorMain",
+    instance: new Storage({
+      area: "local"
+    })
+  })
+
+  const [colorSecond, setColorSecond] = useStorage<string>({
+    key: "fontColorSecond",
+    instance: new Storage({
+      area: "local"
+    })
+  })
+
+  const [imageUrl, setImageUrl] = useStorage<string>({
+    key: "imageUrl",
+    instance: new Storage({
+      area: "local"
+    })
+  })
+
   const [isChecked, setIsChecked] = useStorage({
     key: "gifPFP",
     instance: new Storage({
@@ -12,9 +42,30 @@ function IndexPopup() {
     })
   })
 
+  const [isInnerChecked, setIsInnerChecked] = useStorage({
+    key: "innerBackground",
+    instance: new Storage({
+      area: "local"
+    })
+  })
+
+  const imageUrlText = useRef<HTMLInputElement>(null)
+
   const handleCheckboxChange = async (event) => {
     const newValue = event.target.checked
     setIsChecked(newValue)
+  }
+
+  const handleImageUrlChange = async () => {
+    if (imageUrlText.current) {
+      const newValue = imageUrlText.current.value
+      setImageUrl(newValue)
+    }
+  }
+
+  const handleInnerBackgroundChange = async (event) => {
+    const newValue = event.target.checked
+    setIsInnerChecked(newValue)
   }
 
   return (
@@ -26,6 +77,13 @@ function IndexPopup() {
           href="https://warpcast.com/undefined"
           target="_blank">
           @undefined
+        </a>{" "}
+        &{" "}
+        <a
+          className="text-blue-500"
+          href="https://warpcast.com/jumble"
+          target="_blank">
+          @jumble
         </a>
       </div>
       <div className="flex items-center mt-5">
@@ -36,10 +94,58 @@ function IndexPopup() {
           value="gifPFP"
           checked={isChecked}
           onChange={handleCheckboxChange}
+          className="hidden peer"
         />
-        <label className="ml-2" htmlFor="gifPFP">
-          enable gif PFP
+        <label
+          htmlFor="gifPFP"
+          className={`relative flex items-center cursor-pointer border-2 border-gray-300 w-10 h-5 rounded-full transition-colors duration-300 ${isChecked ? "bg-blue-500" : "bg-gray-200"}`}>
+          <span
+            className={`absolute left-1 top-0 w-4 h-4 bg-white border border-gray-300 rounded-full transition-transform duration-300 transform ${isChecked ? "translate-x-4" : ""}`}></span>
         </label>
+        <span className="ml-2">enable gif PFP</span>
+      </div>
+      <div className="flex items-center mt-5 gap-3">
+        <PopoverPicker color={colorMain} onChange={setColorMain} />
+        <label>Main Font Color</label>
+      </div>
+      <div className="flex items-center mt-5 gap-3">
+        <PopoverPicker color={colorSecond} onChange={setColorSecond} />
+        <label>Secondary Font Color</label>
+      </div>
+      <div className="flex items-center mt-5 gap-3">
+        <button
+          className="px-6 py-2 rounded-md font-semibold transition-colors duration-200 ease-in-out bg-blue-600 text-white
+                hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 active:bg-blue-800"
+          onClick={handleImageUrlChange}>
+          Enter
+        </button>
+        <input
+          type="text"
+          ref={imageUrlText}
+          className="px-4 py-2 border rounded-md outline-none transition-colors duration-200 ease-in-out
+                bg-white text-gray-900
+                border-gray-300 focus:ring-2 focus:ring-blue-500
+                focus:border-blue-500"
+          placeholder={imageUrl ? imageUrl : "Image Url"}
+        />
+      </div>
+      <div className="flex items-center mt-5">
+        <input
+          type="checkbox"
+          id="innerBackground"
+          name="option"
+          value="innerBackground"
+          checked={isInnerChecked}
+          onChange={handleInnerBackgroundChange}
+          className="hidden peer"
+        />
+        <label
+          htmlFor="innerBackground"
+          className={`relative flex items-center cursor-pointer border-2 border-gray-300 w-10 h-5 rounded-full transition-colors duration-300 ${isInnerChecked ? "bg-blue-500" : "bg-gray-200"}`}>
+          <span
+            className={`absolute left-1 top-0 w-4 h-4 bg-white border border-gray-300 rounded-full transition-transform duration-300 transform ${isInnerChecked ? "translate-x-4" : ""}`}></span>
+        </label>
+        <span className="ml-2">Inner Background</span>
       </div>
       <br />
     </div>
